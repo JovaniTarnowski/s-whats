812d3978f4ba65c9ebc3575c78804e71ce34c33c
JtMegazord <jovanitarnowski@gmail.com>
2019-09-09 15:37:10 -0300

    Formated lines:
    45, 46 added lamina_fundo

--- w-msg.py
+++ w-msg.py
@@ -3,12 +3,14 @@
 # Handles "Guia de fundos" with pandas them format FUnd`s name to put on GUI.
 
 # =====================================================================================
+
+# import numpy.random.common
+# import numpy.random.bounded_integers
+# import numpy.random.entropy
 import PySimpleGUI as sg
 import pandas as pd
 from easygui import *
-import numpy.random.common
-import numpy.random.bounded_integers
-import numpy.random.entropy
+
 
 df = pd.read_csv(
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vRAAEPTLdtgK6tmAAOffsYt69zSBtNJpGSMWe6-Jaz6Zh-YJrGtZuEechkkT_6BIY4Ou6Hr0p2CTgi2/pub?output=csv")
@@ -40,25 +42,28 @@ for fundo in list(choice):
     df4 = df['Liquidez_total_(CotizaçãoLiquidação)'][fundo]
     df5 = df.Tributação[fundo]
     df6 = df.CVM[fundo]
-
+    df7 = df.Lamina_fundo[fundo]
+    print(df7)
     if df5 == 'Renda Variável':
         sg.PopupScrolled(
-"""Fundo: *{0}*
+"""Fundo: *{0}* 
 *Rentabilidade*: IBOV + X (início)
 *IR*: incidirá no momento do resgate, à alíquota de 15/% sobre o lucro obtido.
 *Liquidez*: D+{1}
 *Categoria*: {2}
-*Aplicacao Minima*: {3}""".format(df1, df4, df6, df2),
+*Aplicacao Minima*: {3}
+*Mais informacoes do fundo*: {4} """.format(df1, df4, df6, df2, df7),
             title='Verificar', yes_no=True)
 
     else:
         sg.PopupScrolled(
-"""Fundo: *{0}*
+"""Fundo: *{0}* 
 *Rentabilidade*: {1} CDI (início)
 *IR*: respeita a tabela regressiva de renda fixa (antecipação via come-cotas) 
 *Liquidez*: D+{2}
 *Categoria*: {3}
-*Aplicacao Minima*: {4}""".format(df1, df3, df4, df6, df2),
+*Aplicacao Minima*: {4}
+*Mais informacoes do fundo*: {5} """.format(df1, df3, df4, df6, df2, df7),
             title='Verificar', yes_no=True)
 
 # =====================================================================================
