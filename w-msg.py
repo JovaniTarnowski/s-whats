42517e427350503bf882f5c2d43550ca7ac261a4
JtMegazord <jovanitarnowski@gmail.com>
2019-09-26 11:20:32 -0300

    Ver. 0,0,2  - Add IBOV+X in line 50 and so on...

--- w-msg.py
+++ w-msg.py
@@ -5,6 +5,7 @@
 # Bugs:
     #install: pip install --default-timeout=100 future
 # =====================================================================================
+
 import PySimpleGUI as sg
 import pandas as pd
 from easygui import *
@@ -26,6 +27,7 @@ choices = []
 for fundo in df['FUNDO']:
     choices.append(fundo)
 
+
 choice = multchoicebox(msg, title, choices)
 
 for fundo in list(choice):
@@ -38,17 +40,18 @@ for fundo in list(choice):
     df5 = df.Tributação[fundo]
     df6 = df.CVM[fundo]
     df7 = str(df.Lamina_fundo[fundo])
+    df8 = df.Acima_IBOV[fundo]
 
     if df7 == "nan":
 
         if df5 == 'Renda Variável':
             sg.PopupScrolled(
 """Fundo: *{0}* 
-*Rentabilidade*: X % (início)
+*Rentabilidade*: IBOV + {4}% (início)
 *IR*: incidirá no momento do resgate, à alíquota de 15/% sobre o lucro obtido.
 *Liquidez*: D+{1}
 *Categoria*: {2}
-*Aplicacao Minima*: {3}""".format(df1, df4, df6, df2), title='Verificar', yes_no=True)
+*Aplicacao Minima*: {3}""".format(df1, df4, df6, df2, df8), title='Verificar', yes_no=True)
 
         elif df5 == 'Debêntures Incentivadas':
             sg.PopupScrolled(
@@ -72,12 +75,12 @@ for fundo in list(choice):
         if df5 == 'Renda Variável':
             sg.PopupScrolled(
 """Fundo: *{0}* 
-*Rentabilidade*: X % (início)
+*Rentabilidade*: IBOV + {5}% (início)
 *IR*: incidirá no momento do resgate, à alíquota de 15/% sobre o lucro obtido.
 *Liquidez*: D+{1}
 *Categoria*: {2}
 *Aplicacao Minima*: {3}
-*Mais informacoes do fundo*: {4} """.format(df1, df4, df6, df2, df7), title='Verificar', yes_no=True)
+*Mais informacoes do fundo*: {4} """.format(df1, df4, df6, df2, df7, df8), title='Verificar', yes_no=True)
 
         elif df5 == 'Debêntures Incentivadas':
             sg.PopupScrolled(
